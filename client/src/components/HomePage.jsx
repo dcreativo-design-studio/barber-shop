7f816b1e69e1e23b86481fb2006ad90a11034932
dcreativo-design-studio <info@dcreativo.ch>
2025-03-09 12:45:53 +0100

    upgrade hero-image7.0

--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -25,7 +25,7 @@ const HomePage = React.memo(() => {
       className="hero-image high-quality-image"
       style={{
         backgroundImage: 'url("/shot-img2.png")',
-        backgroundPosition: 'center',
+        backgroundPosition: 'left center',
       }}
     ></div>
 
